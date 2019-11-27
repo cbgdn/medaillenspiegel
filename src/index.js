0f854f11e831f66380e8f19a0ef3e8b67b36607e
Art4 <art4@wlabs.de>
2019-11-27 20:26:19 +0100

    Bootstrap installiert

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import css from '../assets/css/style.css';
 import app from './app.js';
 
--- webpack.config.js
+++ webpack.config.js
@@ -9,8 +9,11 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /\.css/,
-                use: ['style-loader', 'css-loader']
+                test: /\.(css)$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                ]
             }
         ]
     }
